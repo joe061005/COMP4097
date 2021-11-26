ad0fe7ddc3254abbd5edaf037a3241004af88570
HKBUSeanTYH <18200265@life.hkbu.edu.hk>
2021-11-26 14:46:06 +0800

    refactored code to use individual views instead of content view

---
+++ SeeFood/HomeView.swift
@@ -0,0 +1,116 @@
+//
+//  HomeView.swift
+//  SeeFood
+//
+//  Created by xdeveloper on 26/11/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State var isPresenting: Bool = false
+    @State var uiImage: UIImage?
+    @State var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
+    
+    @ObservedObject var classifier: ImageClassifier
+    
+    @State var food: Food = Food(foods: [])
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Image(systemName: "photo")
+                    .onTapGesture {
+                        isPresenting = true
+                        sourceType = .photoLibrary
+                    }
+                
+                Image(systemName: "camera")
+                    .onTapGesture {
+                        isPresenting = true
+                        sourceType = .camera
+                    }
+            }
+            .font(.title)
+            .foregroundColor(.blue)
+            
+            Rectangle()
+                .strokeBorder()
+                .foregroundColor(.yellow)
+                .overlay(
+                    Group {
+                        VStack{
+                            if uiImage != nil {
+                                Image(uiImage: uiImage!)
+                                    .resizable()
+                                    .scaledToFit()
+                            }
+                        }
+                    }
+                )
+            
+            VStack{
+                if(!food.foods.isEmpty){
+                    List(food.foods[0].foodNutrients){
+                        nut in
+                        Text("\(nut.nutrientName): \(nut.nutrientNumber)\(nut.unitName)")
+                    }
+                }
+            }
+            
+            
+            
+            VStack{
+                Button(action: {
+                    if uiImage != nil {
+                        classifier.detect(uiImage: uiImage!)
+                    }
+                }) {
+                    Image(systemName: "bolt.fill")
+                        .foregroundColor(.orange)
+                        .font(.title)
+                }
+                
+                
+                Group {
+                    if let imageClass = classifier.imageClass {
+                        HStack{
+                            Text("Image categories:")
+                                .font(.caption)
+                            Text(imageClass)
+                                .bold()
+                        }
+                    } else {
+                        HStack{
+                            Text("Image categories: NA")
+                                .font(.caption)
+                        }
+                    }
+                }
+                .font(.subheadline)
+                .padding()
+                
+            }
+        }
+        
+        .sheet(isPresented: $isPresenting){
+            ImagePicker(uiImage: $uiImage, isPresenting:  $isPresenting, sourceType: $sourceType)
+                .onDisappear{
+                    if uiImage != nil {
+                        classifier.detect(uiImage: uiImage!)
+                        classifier.getNutritionInfo(){
+                            food = classifier.food!
+                        }
+                    }
+                }
+            
+        }.padding()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(classifier: ImageClassifier())
+    }
+}
--- SeeFood/SeeFoodApp.swift
+++ SeeFood/SeeFoodApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SeeFoodApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(classifier: ImageClassifier())
+            ContentView()
         }
     }
 }
