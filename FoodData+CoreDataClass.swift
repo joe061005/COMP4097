6702fc2ce60f83a346c075b8e7bb917821a33820
HKBUSeanTYH <18200265@life.hkbu.edu.hk>
2021-11-28 13:48:24 +0800

    added persistence and core data

---
+++ FoodData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FoodData+CoreDataClass.swift
+//  SeeFood
+//
+//  Created by xdeveloper on 28/11/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FoodData)
+public class FoodData: NSManagedObject {
+
+}
