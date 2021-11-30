78762777f7fb725fe60ccfab18ed6c1fd3be72e0
HKBUSeanTYH <18200265@life.hkbu.edu.hk>
2021-11-30 13:40:06 +0800

    added the foreach loop

--- SeeFood/NavigatedNutritionView.swift
+++ SeeFood/NavigatedNutritionView.swift
@@ -13,14 +13,33 @@ struct NavigatedNutritionView: View {
     var body: some View {
         VStack{
             Text(nutritionData?.timestamp ?? "no value found")
-            Text(nutritionData?.name?[1] ?? "no value found")
+            ForEach(0 ..< (nutritionData?.name?.count)!) { value in
+                Group{
+                    Text("Detected: \(nutritionData?.name?[value] ?? "no value found")")
+                    Text("Sugar: \(nutritionData?.sugar_g?[value] ?? -1, specifier: "%.1f") g")
+                    Text("Fiber: \(nutritionData?.fiber_g?[value] ?? -1, specifier: "%.1f") g")
+                    Text("Carbohydrates: \(nutritionData?.carbohydrates_total_g?[value] ?? -1, specifier: "%.1f") g")
+                    Text("Serving size: \(nutritionData?.serving_size_g?[value] ?? -1, specifier: "%.1f") g")
+                    Text("Sodium: \(nutritionData?.sodium_mg?[value] ?? -1, specifier: "%.1f") mg")
+                }
+                
+                Group{
+                    Text("Potassium: \(nutritionData?.potassium_mg?[value] ?? -1, specifier: "%.1f") mg")
+                    Text("Saturated Fat: \(nutritionData?.fat_saturated_g?[value] ?? -1, specifier: "%.1f") g")
+                    Text("Total Fat: \(nutritionData?.fat_total_g?[value] ?? -1, specifier: "%.1f") g")
+                    Text("Calories: \(nutritionData?.calories?[value] ?? -1, specifier: "%.1f") cal")
+                    Text("Cholesterol: \(nutritionData?.cholestrol_mg?[value] ?? -1, specifier: "%.1f") mg")
+                    Text("Protein: \(nutritionData?.protein_g?[value] ?? -1, specifier: "%.1f") g")
+                }
+            }
+            
         }
         
     }
 }
 
-struct NavigatedNutritionView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigatedNutritionView(nutritionData: nil)
-    }
-}
+//struct NavigatedNutritionView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        NavigatedNutritionView(nutritionData: nil)
+//    }
+//}
